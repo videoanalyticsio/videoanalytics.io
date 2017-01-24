45359f03b3cff9d76070b0ce249cf466f3c1fb8e
Adam Stankiewicz <agstanki@gmail.com>
2017-01-24 17:11:31 -0500

    Added support for custom reporting of video analytics events in callback function, default configuration file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import _ from 'lodash';
 
+import DEFAULTS from './config.defaults';
+
 class VideoAnalyticsIO {
-  constructor(options) {
-    const config = options || {};
-
-    const DEFAULT_EVENTS_TO_TRACK = [
-      'loaded', // done
-      'end', // done
-      'seek', // in progress
-      'play', // done
-      'pause', // done
-      'volumeChange', // done
-    ];
-    const DEFAULT_NUM_DECIMAL_PLACES = 3;
-    const DEFAULT_NUM_EVENTS_BEFORE_SENDING = 25;
-
-    this.numEventsBeforeSending =
-      config.numEventsBeforeSending || DEFAULT_NUM_EVENTS_BEFORE_SENDING;
-    this.eventsToTrack = config.eventsToTrack || DEFAULT_EVENTS_TO_TRACK;
-    this.numDecimalPlaces = config.numDecimalPlaces || DEFAULT_NUM_DECIMAL_PLACES;
+  constructor() {
     this.videoPlayers = [];
   }
 
-  init() {
+  init(options) {
+    const config = options || DEFAULTS;
+
+    this.numEventsBeforeSending =
+      config.NUM_EVENTS_BEFORE_SENDING || DEFAULTS.NUM_EVENTS_BEFORE_SENDING;
+    this.eventsToTrack = config.EVENTS_TO_TRACK || DEFAULTS.EVENTS_TO_TRACK;
+    this.numDecimalPlaces = config.NUM_DECIMAL_PLACES || DEFAULTS.NUM_DECIMAL_PLACES;
+    this.dataCallback = config.DATA_CALLBACK || DEFAULTS.DATA_CALLBACK;
+
     this.getAllVideoPlayers();
     this.setEventListeners();
   }
@@ -31,15 +24,11 @@ class VideoAnalyticsIO {
     _.forEach(document.getElementsByTagName('video'), (el) => {
       const obj = {
         el,
+        title: '',
         ready: false,
         currentTime: 0,
         currentVolume: 1,
         duration: 0,
-        seeking: {
-          active: false,
-          from: 0,
-          to: 0,
-        },
         events: [],
       };
       this.videoPlayers.push(obj);
@@ -49,34 +38,42 @@ class VideoAnalyticsIO {
   setEventListeners() {
     _.forEach(this.videoPlayers, (obj, index) => {
       if (_.indexOf(this.eventsToTrack, 'loaded') >= 0) {
-        obj.el.addEventListener('loadedmetadata', event => this.handleEvent('loaded', event, index));
+        const handleLoaded = (i) => {
+          this.handleEvent('loaded', i);
+        };
+        obj.el.addEventListener('loadedmetadata', () => handleLoaded(index));
+
+        // Check video readyState if metadata is loaded before the loadedmetadata event fires
+        if (obj.el.readyState >= 1) {
+          handleLoaded(index);
+        }
       }
 
       if (_.indexOf(this.eventsToTrack, 'play') >= 0) {
-        obj.el.addEventListener('play', event => this.handleEvent('play', event, index));
+        obj.el.addEventListener('play', () => this.handleEvent('play', index));
       }
 
       if (_.indexOf(this.eventsToTrack, 'pause') >= 0) {
-        obj.el.addEventListener('pause', event => this.handleEvent('pause', event, index));
+        obj.el.addEventListener('pause', () => this.handleEvent('pause', index));
       }
 
       if (_.indexOf(this.eventsToTrack, 'end') >= 0) {
-        obj.el.addEventListener('ended', event => this.handleEvent('end', event, index));
+        obj.el.addEventListener('ended', () => this.handleEvent('end', index));
       }
 
       if (_.indexOf(this.eventsToTrack, 'seek') >= 0) {
-        obj.el.addEventListener('seeked', event => this.handleEvent('seeked', event, index));
+        obj.el.addEventListener('seeked', () => this.handleEvent('seeked', index));
       }
 
       if (_.indexOf(this.eventsToTrack, 'volumeChange') >= 0) {
-        obj.el.addEventListener('volumechange', event => this.handleEvent('volumeChange', event, index));
+        obj.el.addEventListener('volumechange', () => this.handleEvent('volumeChange', index));
       }
 
       obj.el.addEventListener('timeupdate', () => this.timeUpdate(index));
     });
   }
 
-  handleEvent(label, event, index) {
+  handleEvent(label, index) {
     const video = this.videoPlayers[index];
     let append = true;
     if (label === 'loaded') {
@@ -115,10 +112,6 @@ class VideoAnalyticsIO {
       this.videoPlayers[index].events = _.dropRightWhile(this.videoPlayers[index].events, ['type', 'pause']);
     }
 
-    if (label === 'play') {
-      this.videoPlayers[index].seeking.active = false;
-    }
-
     if (append) {
       this.videoPlayers[index].events.push(eventObj);
       const numEvents = _.size(this.videoPlayers[index].events);
@@ -129,8 +122,17 @@ class VideoAnalyticsIO {
   }
 
   sendEventsForVideo(index) {
-    // TODO send events to server
-    this.resetVideoEvents(index);
+    const obj = {
+      meta: {
+        title: this.videoPlayers[index].title,
+        duration: this.videoPlayers[index].duration,
+      },
+      events: this.videoPlayers[index].events,
+    };
+    this.dataCallback(obj, (error) => {
+      if (error) throw error;
+      this.resetVideoEvents(index);
+    });
   }
 
   resetVideoEvents(index) {
@@ -142,6 +144,11 @@ class VideoAnalyticsIO {
     this.videoPlayers[index].duration =
       +(this.videoPlayers[index].el.duration.toFixed(this.numDecimalPlaces));
     this.videoPlayers[index].currentVolume = this.videoPlayers[index].el.volume;
+
+    // Get the video title with the data-title attribute or "guess" it by using the path
+    // to the video, extracting out the file extension (e.g., examples/videos/big_buck_bunny).
+    this.videoPlayers[index].title = this.videoPlayers[index].el.getAttribute('data-title') ||
+      this.videoPlayers[index].el.currentSrc.split('/').slice(3).join('/').replace(/\.(\w{3,4})(\?.*)?$/i, '');
   }
 
   timeUpdate(index) {
